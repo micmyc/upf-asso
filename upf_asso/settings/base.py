6601dd7cd0afcbc31e627648631c23dd72a8f5c7
Michel Myc <michel.myc@gmail.com>
2026-01-09 10:38:10 +0100

    suppression sqllite

--- upf_asso/settings/base.py
+++ upf_asso/settings/base.py
@@ -98,17 +98,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "upf_asso.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
